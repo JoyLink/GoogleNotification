86efa1cc735e552df7addd2fea860cb3e57a2c7d
Joy <joylinkyizhuo@gmail.com>
2016-11-03 07:44:21 -0700

    search keyword apple from New York Times and Mercurynews successfully

--- playwithAPIs.py
+++ playwithAPIs.py
@@ -1,3 +1,15 @@
-from craigslist import CraigslistJobs
+from googleapiclient.discovery import build
+import pprint
 
-print 'hi'
+my_api_key = ""
+my_cse_id = ""
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
+
+results = google_search(
+    'apple', my_api_key, my_cse_id, num=10)
+for result in results:
+    pprint.pprint(result)
